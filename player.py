81aa510f93c71846d9f54ba45d33a34018ac495d
Victor Hugo <d.hugo@aluno.ifsp.edu.br>
2026-04-26 22:33:07 -0300

    feat(player): binary search to find n_start

--- player.py
+++ player.py
@@ -1,69 +1,67 @@
 ### TODO: PREENCHA SUAS INFORMAÇÕES AQUI ###
-# Nome #01 (quem entregou o código):    [NOME COMPLETO #01] 
+# Nome #01 (quem entregou o código):    [NOME COMPLETO #01]
 # RA #01 (quem entregou o código):      [RA #01]
 # Nome #02:                             [NOME COMPLETO #02]
 # RA #02:                               [RA #02]
 
-"""Implemente aqui o seu código para adivinhar a regra.
+## types:
+guess_type = {"NUMBER": "NUMBER", "RULE": "RULE"}
+number_direction_type = {"GREATER": "maior", "LESS": "menor"}
 
-Seu principal objetivo é implementar a função `player`, que deve retornar sua ação na rodada (chute de número ou chute de regra) e seu chute.
-1. Se for um chute de número, ele deve ser um inteiro entre 1 e 100.000.
-2. Se for um chute de regra, ele deve ser uma lista do tipo [TIPO, P1, P2], onde:
-    - TIPO é uma string que pode ser "mod", "pot" ou "int", indicando o tipo da regra;
-    - P1 e P2 são os parâmetros (números inteiros) da regra, que dependem do tipo.
-        - Se TIPO for "mod", P1 é o valor de k e P2 é o valor de r.
-        - Se TIPO for "pot", P1 é o valor de p. P2 é ignorado e pode ser qualquer valor.
-        - Se TIPO for "int", P1 é o valor de a e P2 é o valor de b.
+## rules control set:
+CAN_BE_PERFECT_POW = True
+CAN_BE_MOD = True
+CAN_BE_INTERVAL = True
 
-Exemplos de retornos válidos da função `player`:
-- ["NUMBER", 42]             Chutando o número 42
-- ["NUMBER", 100000]         Chutando o número 100.000
-- ["RULE", ["mod", 3, 1]]    Chutando a regra "n mod 3 dá resto 1"
-- ["RULE", ["pot", 2, 999]]  Chutando a regra "n é potência perfeita de ordem 2"
-- ["RULE", ["int", 10, 20]]  Chutando a regra "n pertence ao intervalo [10, 20]"
+## first n found:
+n_start = -1
+left = -1
+right = -1
 
-Caso sua função não tenha um retorno adequado, a automatização não irá ocorrer tanto em game.py quanto em tournament.py.
+## DEBUG SECTION
+global attempts
+attempts = -1
 
----
-
-A função `player` recebe duas listas como argumentos:
-- number_guesses: lista de respostas aos chutes de número anteriores, onde cada elemento é uma lista do tipo [chute, direção, acerto], sendo:
-    - chute:            o número inteiro chutado
-    - direção:          a direção que indica se um número mais próximo que satisfaz a regra é maior ou menor do que o chute,
-        sendo "igual" se o chute satisfizer a regra e menor se o chute estiver exatamente entre dois números que satisfazem a regra
-    - acerto:           booleano indicando se o chute satisfaz a regra ou não
-
-- rule_guesses: lista de respostas aos chutes de regras anteriores, onde cada elemento é uma lista do tipo [TIPO, P1, P2], 
-    que significam a mesma coisa que os elementos do chute de regra descritos mais acima
-
-Você pode implementar outras funções para auxiliar a função `player` e salvar informações entre os chutes usando variáveis globais (fora de qualquer função).
-
-Para mais informações, verifique o README.md ou consulte um monitor.
-"""
-
-import random
-
-CHUTE_DE_NUMERO = "NUMBER"
-CHUTE_DE_REGRA = "RULE"
 
 def player(number_guesses, rule_guesses):
-    """Função principal do jogador. 
-    
+    """Função principal do jogador.
+
     Exemplo de estratégia: chutar regras aleatórias.
     """
-    
-    TIPO = random.choice(["mod", "pot", "int"])
-    
-    if TIPO == "mod":
-        k = random.randint(2, 100)
-        r = random.randint(0, k - 1)
-        chute = [TIPO, k, r]
-    elif TIPO == "pot":
-        p = random.randint(2, 10)
-        chute = [TIPO, p, 0]
-    else:
-        a = random.randint(1, 100_000) # Dica: o underline (_) pode ser usado para melhorar a legibilidade de números grandes em Python!
-        b = random.randint(a, min(100_000, a + 100))
-        chute = [TIPO, a, b]
-    
-    return [CHUTE_DE_REGRA, chute]
+    global attempts, n_start, left, right
+    global CAN_BE_PERFECT_POW, CAN_BE_MOD, CAN_BE_INTERVAL
+    global guess_type, number_direction_type
+
+    attempts += 1
+
+    print(f"Attempt #{attempts}")
+    print(f"Number guesses: {number_guesses}")
+    print(f"Rule guesses: {rule_guesses}")
+    print()
+
+    if n_start == -1:
+        if len(number_guesses) and number_guesses[-1][2]:
+            n_start = number_guesses[-1][0]
+        else:
+            if left == -1:
+                left = 1
+                return [guess_type["NUMBER"], 1]
+
+            if right == -1:
+                right = 100000
+                return [guess_type["NUMBER"], 100000]
+
+            last_guess, direction, _ = number_guesses[-1]
+
+            if direction == number_direction_type["LESS"]:
+                right = last_guess - 1
+
+            elif direction == number_direction_type["GREATER"]:
+                left = last_guess + 1
+
+            return [guess_type["NUMBER"], (left + right) // 2]
+
+    print("n_start find: ", n_start)
+    print("with attempts: ", attempts)
+
+    return ["TODO", 0]
