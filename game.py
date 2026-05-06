c2a095d08ad87941208aa83837f48a3dd721d011
mgcvale <miguelcvalealt@gmail.com>
2026-05-06 00:25:19 -0300

    add base mod solution with decision tree

--- game.py
+++ game.py
@@ -86,6 +86,7 @@ def choose_rule():
     Retorna função que representa a regra, uma descrição e um dicionário com nome da regra e seus números.
     """
     rule_type = random.choice(["mod", "pot", "int"])
+    rule_type = "mod"
     if rule_type == "mod":
         k = random.randint(2, 100)
         r = random.randint(0, k - 1)
